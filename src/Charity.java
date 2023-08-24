cf053a0ff427c14fb842f488a83287f84cd773ab
wedelos21 <143046916+wedelos21@users.noreply.github.com>
2023-08-23 22:14:07 -0500

    Trial Please See

--- src/Charity.java
+++ src/Charity.java
@@ -1,3 +1,11 @@
 public class Charity {
-    
+    private String CharityName;
+    private String Description;
+    private float DonationAmount;
+
+    public Charity(String charityName, String description, float donationAmount) {
+        CharityName = charityName;
+        Description = description;
+        DonationAmount = donationAmount;
+    }
 }
